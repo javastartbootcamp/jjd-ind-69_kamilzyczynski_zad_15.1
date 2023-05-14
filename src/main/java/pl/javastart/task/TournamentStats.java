fd88f82dc80f5c6218b19b6fd1a9dd0c9589abb1
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-05-14 11:54:01 +0200

    Poprawione rozwiązanie

--- src/main/java/pl/javastart/task/TournamentStats.java
+++ src/main/java/pl/javastart/task/TournamentStats.java
@@ -9,18 +9,18 @@ public class TournamentStats {
     public static final String FIRST_NAME = "1";
     public static final String LAST_NAME = "2";
     public static final String SCORE = "3";
-    List<Player> players = new ArrayList<>();
 
     void run(Scanner scanner) throws IOException {
         // tutaj dodaj swoje rozwiązanie
         // użyj przekazanego scannera do wczytywania wartości
-        getPlayers(scanner);
-        chooseSortMethod(scanner);
-        save();
+        List<Player> players = getPlayers(scanner);
+        Comparator<Player> comparator = chooseSortMethod(scanner);
+        Collections.sort(players, comparator);
+        save(players);
         System.out.println("Dane posortowano i zapisano do pliku stats.csv");
     }
 
-    private void chooseSortMethod(Scanner scanner) {
+    private Comparator<Player> chooseSortMethod(Scanner scanner) {
         System.out.printf("Po jakim parametrze posortować? (%s - imię, %s - nazwisko, %s - wynik) \n", FIRST_NAME,
                 LAST_NAME, SCORE);
         String choice = scanner.nextLine();
@@ -37,16 +37,17 @@ public class TournamentStats {
                 }
             }
         } while (!stopAsking);
-        Collections.sort(players, comparator);
 
         System.out.println("Sortować rosnąco czy malejąco? (1 - rosnąco, 2 - malejąco)");
         int sortChoice = scanner.nextInt();
         if (sortChoice == 2) {
-            Collections.reverse(players);
+            comparator = comparator.reversed();
         }
+        return comparator;
     }
 
-    private void getPlayers(Scanner scanner) {
+    private List<Player> getPlayers(Scanner scanner) {
+        List<Player> players = new ArrayList<>();
         boolean stopAsking = false;
         do {
             System.out.println("Podaj wynik kolejnego gracza (lub stop):");
@@ -59,9 +60,10 @@ public class TournamentStats {
                 players.add(new Player(split[0], split[1], Integer.parseInt(split[2])));
             }
         } while (!stopAsking);
+        return players;
     }
 
-    private void save() throws IOException {
+    private void save(List<Player> players) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("stats.csv"));
         for (Player player : players) {
             writer.write(player.getFirstName() + " " + player.getLastName() + ";" + player.getScore());
